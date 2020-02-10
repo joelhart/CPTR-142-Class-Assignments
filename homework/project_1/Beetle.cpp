b1d204e0b8ece8184adc138833118a2de5688cb0
hartjo <hartjo@cs.wallawalla.edu>
2020-02-10 02:10:05 +0000

    autosave

--- homework/project_1/Beetle.cpp
+++ homework/project_1/Beetle.cpp
@@ -5,74 +5,91 @@ Name: hartjo
 Date: 2/3
  */
 
- #include <iostream>
- #include "Beetle.h"
- #include "Dice.h"
- #include <vector>
- #include <string>
+#include "Beetle.h"
+#include "Dice.h"
+#include <iostream>
+#include <string>
+#include <vector>
 
- using namespace std;
+using namespace std;
 
- int main() {
+int main() {
 
-     int playerNum = 0;
-     int seed = 0;
+  int playerNum = 0;
+  int seed = 0;
 
-     start(seed, playerNum);
+  start(seed, playerNum);
 
-     vector<Player> nameList(playerNum);
+  vector<Player> nameList(playerNum);
 
-     fillPlayerList(nameList, playerNum);
+  fillPlayerList(nameList, playerNum);
 
-     play(nameList, playerNum, seed);
+  play(nameList, playerNum, seed);
 
-     return 0;
- }
+  return 0;
+}
 
- void start(int &seed, int &playerNum) {
+void start(int &seed, int &playerNum) {
 
-     cout << "Enter number of players: ";
-     cin >> playerNum;
+  cout << "Enter number of players: ";
+  cin >> playerNum;
+  while (1) {
+    if (cin.fail()) {
+      cin.clear();
+      cin.ignore(100, '\n');
+      cout << "Incorrect Input: Please try again: " << endl;
+      cin >> playerNum;
+    }
+    if (!cin.fail())
+      break;
+  }
 
-     cout << "Enter seed: ";
-     cin >> seed;
+  cout << "Enter seed: ";
+  cin >> seed;
+  while (1) {
+    if (cin.fail()) {
+      cin.clear();
+      cin.ignore(100, '\n');
+      cout << "Incorrect Input: Please try again: " << endl;
+      cin >> seed;
+    }
+    if (!cin.fail())
+      break;
+  }
+}
 
- }
+void fillPlayerList(vector<Player> &nameList, int size) {
 
- void fillPlayerList(vector<Player> &nameList, int size) {
+  for (int i = 0; i < size; i++) {
+    Player tempHolder = nameList.at(i);
 
-     for (int i = 0;i < size;i++) {
-         Player tempHolder = nameList.at(i);
+    cout << "Enter player #";
+    cout << i + 1;
+    cout << "'s name: ";
+    cin >> tempHolder.playerName;
 
-         cout << "Enter player #";
-         cout << i+1;
-         cout << "'s name: ";
-         cin >> tempHolder.playerName;
+    nameList.at(i) = tempHolder;
+  }
+}
 
-         nameList.at(i) = tempHolder;
-     }
+void play(vector<Player> &who, int size, int seed) {
 
- }
+  int turnCount = 0;
+  Dice roll(seed);
+  int rollHold = 0;
+  bool continueRound = true;
 
- void play(vector<Player> &who, int size, int seed) {
+  do {
+    for (int i = 0; i < who.size(); i++) {
+      turnCount++;
+      rollHold = roll.diceRoll();
 
-     int turnCount = 0;
-     Dice roll(seed);
-     int rollHold = 0;
-     bool continueRound = true;
-
-     do {
-            for (int i = 0; i < who.size(); i++) {
-                turnCount++;
-                rollHold = roll.diceRoll();
-
-                if (who[i].playerBody(rollHold)) {
-                    cout << who[i].playerName << " is the winner! It took " << turnCount << " turns to win." << endl;
-                    continueRound = false;
-                    break;
-                }
-            }
-     } while(continueRound);
- }
-
- 
+      if (who[i].playerBody(rollHold)) {
+        cout << who[i].playerName << " is the winner! It took " << turnCount
+             << " turns to win." << endl;
+        continueRound = false;
+        break;
+      }
+    }
+  } while (continueRound);
+}
