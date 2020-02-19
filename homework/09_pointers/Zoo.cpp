6bb46e0f5a20ad1d56cea6234c8cf41fc159f650
hartjo <hartjo@cs.wallawalla.edu>
2020-02-19 05:11:27 +0000

    autosave

--- homework/09_pointers/Zoo.cpp
+++ homework/09_pointers/Zoo.cpp
@@ -11,6 +11,10 @@
 
 // #include "Creature.h"
 #include "Zoo.h"
+#include "Bee.h"
+#include "Cat.h"
+#include "Duck.h"
+#include "Snail.h"
 #include <iomanip>
 #include <iostream>
 #include <string>
@@ -20,18 +24,79 @@ using namespace std;
 
 void Zoo::addCreature(CREATURES creatureType, string creatureName) {
 
-  Creature *ptr = nullptr;
+  if (creatureType == BEE) {
 
-  creatures.push_back(ptr);
+    // Bee newBee(creatureName);
 
-  *ptr = creatureName;
+    // Bee *ptr = nullptr;
+
+    // ptr = &newBee;
+
+    creatures.push_back(new Bee(creatureName));
+  }
+
+  if (creatureType == CAT) {
+
+    // Cat newCat(creatureName);
+
+    // Cat *ptr = nullptr;
+
+    // ptr = &newCat;
+
+    creatures.push_back(new Cat(creatureName));
+  }
+
+  if (creatureType == DUCK) {
+
+    // Duck newDuck(creatureName);
+
+    // Duck *ptr = nullptr;
+
+    // ptr = &newDuck;
+
+    creatures.push_back(new Duck(creatureName));
+  }
+
+  if (creatureType == SNAIL) {
+
+    // Snail newSnail(creatureName);
+
+    // Snail *ptr = nullptr;
+
+    // ptr = &newSnail;
+
+    creatures.push_back(new Snail(creatureName));
+  }
 }
 
 void Zoo::printCensus() {
 
+  cout << "Zoo Census:" << endl;
+
   for (int i = 0; i < creatures.size(); i++) {
-    cout << creatures.at(i) << endl;
+
+    Creature *ptr = nullptr;
+
+    ptr = creatures.at(i);
+
+    cout << " - " << ptr->getName();
+
+    cout << " (" << ptr->getDescription() << ")" << endl;
   }
 }
 
-int getNumberOfLegs() {}
+int Zoo::getNumberOfLegs() {
+
+  int totalLegs = 0;
+
+  for (int i = 0; i < creatures.size(); i++) {
+
+    Creature *ptr = nullptr;
+
+    ptr = creatures.at(i);
+
+    totalLegs += ptr->getNumberOfLegs();
+  }
+
+  return totalLegs;
+}
