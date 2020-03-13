54f23feef96853fd6712f713ecd8bdff9804d3c9
hartjo <hartjo@cs.wallawalla.edu>
2020-03-13 15:28:10 +0000

    autosave

--- homework/10_dynamic_arrays/main.cpp
+++ homework/10_dynamic_arrays/main.cpp
@@ -3,48 +3,26 @@
  * Homework Assignment:  Merge strings
  *
  * File Name:  main.cpp
- * Name:       Joel Hartman
+ * Name:       ?
  * Course:     CPTR 142
- * Date:       3/13
+ * Date:       ?
  *
  */
 #include "mergeStrings.h"
-#include <cstring>
 #include <iostream>
+#include <string>
+using namespace std;
 
-string mergeStrings(int argc, char *argv[], string userEntry) {
-  int maxSoFar = userEntry.size();
-  int currValue;
-  cout << "argc = " << argc << endl;
-  for (int i = 0; i < argc; i++) {
-    currValue = strlen(argv[i]);
-    cout << "argv[" << i << "]: " << argv[i] << " has a size of " << currValue
-         << endl;
-    if (maxSoFar < currValue) {
-      maxSoFar = currValue;
-    }
-  }
+int main(int argc, char *argv[]) {
+  // user input
+  cout << "Enter line to merge: ";
+  string userEntry;
+  getline(cin, userEntry);
 
-  cout << "Max length is: " << maxSoFar << endl;
-  cout << "Number of input strings: " << argc + 1 << endl;
-  cout << "The length of the output string in characters is "
-       << maxSoFar * (argc + 1) << endl;
-  char *resultString = new char[maxSoFar * (argc + 1)];
-  int k = 0;
-  for (int i = 0; i < maxSoFar; i++) {
-    if (i > userEntry.size() - 1) {
-      resultString[k++] = ' ';
-    } else {
-      resultString[k++] = userEntry.at(i);
-    }
-    for (int j = 0; j < argc; j++) {
-      if (i < strlen(argv[j])) {
-        resultString[k++] = argv[j][i];
-      } else {
-        resultString[k++] = ' ';
-      }
-    }
-  }
-  resultString[(argc + 1) * maxSoFar] = '\0';
-  return resultString;
+  string result = mergeStrings(argc, argv, userEntry);
+
+  // output result
+  cout << "\"" << result << "\" is " << result.size()
+       << " characters in length." << endl;
+  return 0;
 }
--- homework/10_dynamic_arrays/mergeStrings.cpp
+++ homework/10_dynamic_arrays/mergeStrings.cpp
@@ -3,10 +3,49 @@
  * Homework Assignment:  Merge strings
  *
  * File Name:  mergeStrings.cpp
- * Name:       ?
+ * Name:       joel hartman
  * Course:     CPTR 142
- * Date:       ?
+ * Date:       3/13
  *
  */
 
-// TODO Add code here.
+#include "mergeStrings.h"
+#include <cstring>
+#include <iostream>
+
+string mergeStrings(int argc, char *argv[], string userEntry) {
+  int maxSoFar = userEntry.size();
+  int currValue;
+  cout << "argc = " << argc << endl;
+  for (int i = 0; i < argc; i++) {
+    currValue = strlen(argv[i]);
+    cout << "argv[" << i << "]: " << argv[i] << " has a size of " << currValue
+         << endl;
+    if (maxSoFar < currValue) {
+      maxSoFar = currValue;
+    }
+  }
+
+  cout << "Max length is: " << maxSoFar << endl;
+  cout << "Number of input strings: " << argc + 1 << endl;
+  cout << "The length of the output string in characters is "
+       << maxSoFar * (argc + 1) << endl;
+  char *resultString = new char[maxSoFar * (argc + 1)];
+  int k = 0;
+  for (int i = 0; i < maxSoFar; i++) {
+    if (i > userEntry.size() - 1) {
+      resultString[k++] = ' ';
+    } else {
+      resultString[k++] = userEntry.at(i);
+    }
+    for (int j = 0; j < argc; j++) {
+      if (i < strlen(argv[j])) {
+        resultString[k++] = argv[j][i];
+      } else {
+        resultString[k++] = ' ';
+      }
+    }
+  }
+  resultString[(argc + 1) * maxSoFar] = '\0';
+  return resultString;
+}
